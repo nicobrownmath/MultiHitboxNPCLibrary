93d41215e34ef7ce9671576dc57b5e4cb7eb96c1
nico <nicobrownmath@gmail.com>
2022-07-28 10:51:01 -0700

    Javelins

---
+++ MultiHitboxNPCLibraryProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MultiHitboxNPCLibrary
+{
+    public class MultiHitboxNPCLibraryProjectile : GlobalProjectile
+    {
+        public bool badCollision;
+
+        public override void SetDefaults(Projectile projectile)
+        {
+            switch(projectile.type)
+            {
+                case ProjectileID.BoneJavelin:
+                case ProjectileID.Daybreak:
+                    badCollision = true;
+                    break;
+            }
+        }
+    }
+}
+
